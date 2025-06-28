2245d251d3d7a527c478466411e95dbccab9487c
mdrazaulkabir <razaulkabirit@gmail.com>
2025-06-29 00:03:58 +0600

    solve this 4 and 5

--- seriesRelatedProblemSolving.dart
+++ seriesRelatedProblemSolving.dart
@@ -12,14 +12,72 @@ import 'dart:io';
 // }
 
 
-//problem2:  1 to n and add all even number
+// //problem2:  1 to n and add all even number
+// main(){
+//   print("Enter n number:");
+//   int result=0;
+//   int n=int.parse(stdin.readLineSync()!);
+//   for(int i=2;i<=n;i=i+2){
+//      print(i);
+//      result=result+i;
+//   }
+//   print("1 to $n add all even number is : $result");
+// }
+
+
+// //problem3:  1 to n and add all odd number
+// main(){
+//   print("Enter n number:");
+//   int result=0;
+//   int n=int.parse(stdin.readLineSync()!);
+//   for(int i=1;i<=n;i=i+2){
+//      print(i);
+//      result=result+i;
+//   }
+//   print("1 to $n add all odd number is : $result");
+// }
+
+
+// //problem2 and 3:  1 to n and add or even all even number in one programe
+// main(){
+//   print("Enter n number:");
+//   int even=0, odd=0;
+//   int n=int.parse(stdin.readLineSync()!);
+//   for(int i=1;i<=n;i=i+1){
+//     print(i);
+//     if(i%2==0){
+//        even=even+i;
+//     }
+//     else{
+//        odd=odd+i;
+//     }
+//   }
+//   print("1 to $n add all even number is : $even");
+//   print("1 to $n add all odd number is : $odd");
+// }
+
+
+// //problem 4: series 1.5+2.5+3.5+4.5+5.5+6.5....
+// main(){
+//   print("Enter n number:");
+//   double result=0;
+//   int n=int.parse(stdin.readLineSync()!);
+//   for(int i=1;i<=n;i=i+1){
+//      print(i);
+//      result=(result+i+1/2);
+//   }
+//   print("1 to $n add this result is : $result");
+// }
+
+
+//problem 4: series 1/1+1/2+1/3+1/4+1/5+1/6....
 main(){
   print("Enter n number:");
-  int result=0;
+  double result=0;
   int n=int.parse(stdin.readLineSync()!);
-  for(int i=2;i<=n;i=i+2){
+  for(int i=1;i<=n;i=i+1){
      print(i);
-     result=result+i;
+     result=(result+1/i);
   }
-  print("1 to $n add all number is : $result");
+  print("1 to $n add sires number result is : ${result.toStringAsFixed(3)}");
 }
