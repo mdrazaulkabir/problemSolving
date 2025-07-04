c93c99ff517e746b509a20c85aea142cd8449ff4
mdrazaulkabir <razaulkabirit@gmail.com>
2025-07-05 00:16:13 +0600

    pattern problem solve 2 3 4

--- patternProblemSolving.dart
+++ patternProblemSolving.dart
@@ -1,19 +1,81 @@
 import 'dart:io';
 
-//problem 1: 
-//1
-//1 2
-//1 2 3
-main(){
-  print('Enter one number :');
-  int num1=int.parse(stdin.readLineSync()!);
-  int row,column;
-  for(row=1; row<=num1;row++){
-      for(int column=1; column<=row; column++){
-           stdout.write(column); //don't create newline  which like in c programme
-           //print("*");     // create new line automatically 
-      }
-     // print(" ");  
-     stdout.write("\n");
-  }
-}
+// //problem 1: 
+// //1
+// //1 2
+// //1 2 3
+// main(){
+//   print('Enter one number :');
+//   int num1=int.parse(stdin.readLineSync()!);
+//   int row,column;
+//   for(row=1; row<=num1;row++){
+//       for(int column=1; column<=row; column++){
+//            stdout.write("$column "); //don't create newline  which like in c programme
+//            //print("*");          // create new line automatically 
+//       }
+//      // print(" ");  
+//      stdout.write("\n");
+//   }
+// }
+
+
+// //problem 2: 
+// //1
+// //2 2
+// //3 3 3
+// main(){
+//   print('Enter one number :');
+//   int num1=int.parse(stdin.readLineSync()!);
+//   int row,column;
+//   for(row=1; row<=num1;row++){
+//       for(int column=1; column<=row; column++){
+//            stdout.write("$row "); //don't create newline  which like in c programme
+//            //print("*");          // create new line automatically 
+//       }
+//      // print(" ");  
+//      stdout.write("\n");
+//   }
+// }
+
+
+
+// //problem 3: 
+// //*
+// //* *
+// //* * *
+// main(){
+//   print('Enter one number :');
+//   int num1=int.parse(stdin.readLineSync()!);
+//   int row,column;
+//   for(row=1; row<=num1;row++){
+//       for(int column=1; column<=row; column++){
+//            stdout.write("* "); //don't create newline  which like in c programme 
+//            //print("*");       // create new line automatically 
+//       }
+//      // print(" ");  
+//      stdout.write("\n");
+//   }
+// }
+
+
+
+// //problem 4: 
+// //1
+// //1 0
+// //1 0 1
+// main(){
+//   print('Enter one number :');
+//   int num1=int.parse(stdin.readLineSync()!);
+//   int row,column;
+//   for(row=1; row<=num1;row++){
+//       for(int column=1; column<=row; column++){
+//            int value=(column%2==0)? 0:1;
+//            stdout.write("${value} "); //don't create newline  which like in c programme 
+//            //print("*");       // create new line automatically 
+//       }
+//      // print(" ");  
+//      stdout.write("\n");
+//   }
+// }
+
+
